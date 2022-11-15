cbc3991a56d415411938af9662d341f6d74e7b2a
Kai Nagar <kaynagar@gmail.com>
2022-11-15 12:47:35 +0200

    style compose-btn

--- src/cmps/email/EmailFolderList.jsx
+++ src/cmps/email/EmailFolderList.jsx
@@ -2,8 +2,14 @@ import { NavLink } from 'react-router-dom'
 
 export const EmailFolderList = ({ onChangeStatus }) => {
   return (
-    <section className='email-side-menu flex column'>
-      <NavLink replace to={'/email/compose'}>
+    <section className='email-folder-list flex column'>
+      <NavLink className='compose-btn flex aling-center' replace to={'/email/compose'}>
+        <span className='edit-img'>
+          <img
+            src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png'
+            alt='Edit Compose'
+          />
+        </span>
         Compose
       </NavLink>
       <span onClick={() => onChangeStatus('inbox')} className='tab inbox-tab'>
